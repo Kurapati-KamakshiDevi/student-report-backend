dc693987e23fee88caf2d46cce311941b05bfabd
Kurapati-KamakshiDevi <kamakshidevi04@gmail.com>
2025-09-22 21:05:24 +0530

    first commit

---
+++ studentreport-backend/src/main/java/com/example/studentreport_backend/StudentreportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentreport_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentreportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentreportBackendApplication.class, args);
+	}
+
+}
---
+++ studentreport-backend/src/test/java/com/example/studentreport_backend/StudentreportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentreport_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentreportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
